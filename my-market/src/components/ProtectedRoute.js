ce45db74a8ad86b312afb3347ff6ffb189583725
SouMurate04 <somrt2004@gmail.com>
2026-06-07 18:16:05 +0900

    Add loading

--- my-market/src/components/ProtectedRoute.js
+++ my-market/src/components/ProtectedRoute.js
@@ -5,12 +5,21 @@ import { Navigate } from "react-router-dom";
 import { fireAuth } from "../firebase";
 
 export default function ProtectedRoute({ children }) {
-  const [loginUser, setLoginUser] = useState(fireAuth.currentUser);
+  const [loginUser, setLoginUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    onAuthStateChanged(fireAuth, user => {
+    const unsubscribe = onAuthStateChanged(fireAuth, user => {
       setLoginUser(user);
+      setIsLoading(false);
     });
+
+    return unsubscribe;
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   if (!loginUser) {
     return <Navigate to="/login" replace />;
