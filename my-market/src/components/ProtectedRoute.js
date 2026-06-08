cc27c110cb0a6c1ef02432e018ecc55fd2f78b2f
SouMurate04 <somrt2004@gmail.com>
2026-06-08 23:16:32 +0900

    Fix Protection

--- my-market/src/components/ProtectedRoute.js
+++ my-market/src/components/ProtectedRoute.js
@@ -6,15 +6,17 @@ import { fireAuth } from "../firebase";
 
 export default function ProtectedRoute({ children }) {
   const [loginUser, setLoginUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(fireAuth, user => {
       setLoginUser(user);
+      setIsLoading(false);
     });
     return unsubscribe;
   }, []);
 
-  if (!loginUser) {
+  if (!isLoading && !loginUser) {
     return <Navigate to="/login" replace />;
   }
 
